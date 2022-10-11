f10172636c07a633239616a280e8f56fd8e005d1
timazlo <timazloy0444@yandex.ru>
2022-10-11 23:04:36 +0300

    add: fonts

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,43 @@
 <template>
-  <div class="header">
-    <a href="#" class="header__logo">
-      <img src="/img/BotWebest.svg" alt="logo">
-    </a>
-    <div class="header__section">
-      <div class="header-links">
-        <a href="#" class="header-links__item">Учет времени</a>
-        <a href="#" class="header-links__item">Отпуск</a>
-        <a href="#" class="header-links__item">Бот</a>
-      </div>
-      <div class="header-info">
-        <div class="counter-time">
-          <div class="counter-time__item counter-item">
-            <div class="counter-item__text">Неделя:</div>
-            <div class="counter-item__count">24 ч.</div>
-          </div>
-          <div class="counter-time__item counter-item">
-            <div class="counter-item__text">Месяц:</div>
-            <div class="counter-item__count">24 ч.</div>
-          </div>
-          <div class="counter-time__item counter-item">
-            <div class="counter-item__text">Сверхурочно:</div>
-            <div class="counter-item__count">24 ч.</div>
-          </div>
+  <div class="page">
+    <div class="page__header header">
+      <a href="#" class="header__logo">
+        <img src="/img/BotWebest.svg" alt="logo">
+      </a>
+      <div class="header__section">
+        <div class="header-links">
+          <a href="#" class="header-links__item">Учет времени</a>
+          <a href="#" class="header-links__item">Отпуск</a>
+          <a href="#" class="header-links__item">Бот</a>
         </div>
-        <button type="button" class="button-main">
-          <img class="button-icon__img" src="/img/play-circle.svg" alt="icon">
-          <span class="button-main__text">Начать рабочий день</span>
-        </button>
-        <div class="initials">АС</div>
-        <div class="full-name">Алиса Сидоренкова</div>
-        <button class="button-black" type="button">Выйти</button>
+        <div class="header-info">
+          <div class="counter-time">
+            <div class="counter-time__item counter-item">
+              <div class="counter-item__text">Неделя:</div>
+              <div class="counter-item__count">24 ч.</div>
+            </div>
+            <div class="counter-time__item counter-item">
+              <div class="counter-item__text">Месяц:</div>
+              <div class="counter-item__count">24 ч.</div>
+            </div>
+            <div class="counter-time__item counter-item">
+              <div class="counter-item__text">Сверхурочно:</div>
+              <div class="counter-item__count">24 ч.</div>
+            </div>
+          </div>
+          <button type="button" class="button-main">
+            <img class="button-icon__img" src="/img/play-circle.svg" alt="icon">
+            <span class="button-main__text">Начать рабочий день</span>
+          </button>
+          <div class="initials">АС</div>
+          <div class="full-name">Алиса Сидоренкова</div>
+          <button class="button-black" type="button">Выйти</button>
+        </div>
       </div>
-    </div>
 
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -43,6 +46,12 @@
 
 
 <style lang="scss">
+* {
+  font-family: 'GloryRegular';
+  margin: 0;
+  padding: 0;
+}
+
 a {
   text-decoration: none;
 }
@@ -52,7 +61,11 @@ button {
   background: none;
 }
 
-
+.page {
+  &__header {
+    margin-bottom: 63px;
+  }
+}
 
 .button-main {
   background: linear-gradient(89.85deg, #0078FF 0.11%, #02AAF6 99.87%);
@@ -146,7 +159,8 @@ button {
 
 .header {
   display: flex;
-  margin: 0 20px;
+  padding: 0 20px;
+  border-bottom: 1px solid #DFDFDF;
 
   &__logo {
     padding: 20px 0;
@@ -166,6 +180,10 @@ button {
   &__item {
     display: flex;
     padding: 20px 10px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 120%;
+    color: #11203E;
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App'
+import './assets/font.css'
 
 
 createApp(App).mount('#app')
