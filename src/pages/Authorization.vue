f04be33369bc5f0cad6fcf0197c29ec6af5296c5
timazlo <timazloy0444@yandex.ru>
2022-10-11 23:42:40 +0300

    fix: BEM

--- src/pages/Authorization.vue
+++ src/pages/Authorization.vue
@@ -4,19 +4,19 @@
       <div class="image-section__wrapper">
         <img class="logo" src="/img/logo.png" alt="logo">
       </div>
-      <img class="image-section__login-1" src="/img/login/2.svg" alt="img">
-      <img class="image-section__login-2" src="/img/login/13.svg" alt="img">
-      <img class="image-section__login-3" src="/img/login/6.png" alt="img">
-      <img class="image-section__login-4" src="/img/login/1.svg" alt="img">
-      <img class="image-section__login-5" src="/img/login/9.svg" alt="img">
-      <img class="image-section__login-6" src="/img/login/7.svg" alt="img">
-      <img class="image-section__login-7" src="/img/login/11.svg" alt="img">
-      <img class="image-section__login-8" src="/img/login/4.svg" alt="img">
-      <img class="image-section__login-9" src="/img/login/10.svg" alt="img">
-      <img class="image-section__login-10" src="/img/login/8.svg" alt="img">
-      <img class="image-section__login-11" src="/img/login/3.svg" alt="img">
-      <img class="image-section__login-12" src="/img/login/5.svg" alt="img">
-      <img class="image-section__login-13" src="/img/login/12.svg" alt="img">
+      <img class="login-image image-section__login-1" src="/img/login/2.svg" alt="img">
+      <img class="login-image image-section__login-2" src="/img/login/13.svg" alt="img">
+      <img class="login-image image-section__login-3" src="/img/login/6.png" alt="img">
+      <img class="login-image image-section__login-4" src="/img/login/1.svg" alt="img">
+      <img class="login-image image-section__login-5" src="/img/login/9.svg" alt="img">
+      <img class="login-image image-section__login-6" src="/img/login/7.svg" alt="img">
+      <img class="login-image image-section__login-7" src="/img/login/11.svg" alt="img">
+      <img class="login-image image-section__login-8" src="/img/login/4.svg" alt="img">
+      <img class="login-image image-section__login-9" src="/img/login/10.svg" alt="img">
+      <img class="login-image image-section__login-10" src="/img/login/8.svg" alt="img">
+      <img class="login-image image-section__login-11" src="/img/login/3.svg" alt="img">
+      <img class="login-image image-section__login-12" src="/img/login/5.svg" alt="img">
+      <img class="login-image image-section__login-13" src="/img/login/12.svg" alt="img">
     </div>
     <div class="page-login__column login-section">
       <div class="login">
@@ -244,6 +244,10 @@ button {
   position: relative;
 }
 
+.login-image {
+  position: absolute;
+}
+
 .image-section {
   width: 50%;
   height: 100vh;
@@ -254,7 +258,6 @@ button {
   position: relative;
 
   &__login-1 {
-    position: absolute;
     @include icons-adaptive(33, 60, 1024, 1980);
     top: 4.7%;
     left: 5%;
@@ -262,83 +265,71 @@ button {
 
   &__login-2 {
     @include icons-adaptive(39, 70, 1024, 1980);
-    position: absolute;
     top: 4.7%;
     right: 40%;
   }
 
   &__login-3 {
     @include icons-adaptive(44, 80, 1024, 1980);
-    position: absolute;
     top: 4.7%;
     right: 5%;
   }
 
   &__login-4 {
     @include icons-adaptive(18, 33, 1024, 1980);
-    position: absolute;
     top: 14.5%;
     left: 34%;
   }
 
   &__login-5 {
-    position: absolute;
     @include icons-adaptive(39, 70, 1024, 1980);
     top: 20%;
     right: 30%;
   }
 
   &__login-6 {
-    position: absolute;
     @include icons-adaptive(60, 110, 1024, 1980);
     top: 29.5%;
     right: 11%;
   }
 
   &__login-7 {
-    position: absolute;
     @include icons-adaptive(42, 76, 1024, 1980);
     top: 29.8%;
     left: 5.1%;
   }
 
   &__login-8 {
-    position: absolute;
     @include icons-adaptive(36, 66, 1024, 1980);
     bottom: 33%;
     left: 5%;
   }
 
   &__login-9 {
-    position: absolute;
     @include icons-adaptive(39, 70, 1024, 1980);
     bottom: 4.5%;
     left: 5%;
   }
 
   &__login-10 {
-    position: absolute;
     @include icons-adaptive(33, 60, 1024, 1980);
     bottom: 19%;
     left: 30.5%;
   }
 
   &__login-11 {
-    position: absolute;
     @include icons-adaptive(44, 80, 1024, 1980);
     bottom: 7%;
     right: 36.5%;
   }
 
   &__login-12 {
-    position: absolute;
     @include icons-adaptive(42, 76, 1024, 1980);
     bottom: 23.7%;
     right: 18.7%;
   }
 
   &__login-13 {
-    position: absolute;
     @include icons-adaptive(33, 60, 1024, 1980);
     bottom: 4.5%;
     right: 5%;
