2d3a92b24f8cae6124aedabb529f42efbf1fe50d
timazlo <timazloy0444@yandex.ru>
2022-10-14 18:48:41 +0300

    upd: checkbox

--- src/pages/Bot.vue
+++ src/pages/Bot.vue
@@ -3,9 +3,9 @@
     <div class="page__bot bot-page">
       <div class="bot-page__column section-list">
         <div class="section-list__check">
-          <label class="check-all">
-            <input class="check-all__checkbox custom-checkbox" type="checkbox">
-            <div class="check-all__text">Все сотрудники</div>
+          <label class="check-all custom-checkbox">
+            <input class="check-all__checkbox" type="checkbox">
+            <span class="check-all__text">Все сотрудники</span>
           </label>
         </div>
         <Accordion
@@ -74,25 +74,6 @@ export default {
   display: flex;
   gap: 17px;
   cursor: pointer;
-  align-items: center;
-  user-select: none;
-
-  &:before {
-    content: '';
-    display: inline-block;
-    width: 1em;
-    height: 1em;
-    flex-shrink: 0;
-    flex-grow: 0;
-    border: 1px solid #adb5bd;
-    border-radius: 0.25em;
-    margin-right: 0.5em;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 50% 50%;
-  }
-
-
 
   &__checkbox {
     width: 22px;
@@ -105,44 +86,68 @@ export default {
   }
 }
 
-.custom-checkbox {
+.custom-checkbox>input {
   position: absolute;
   z-index: -1;
   opacity: 0;
-
 }
 
-.custom-checkbox:checked .check-all:before {
-  border-color: #0b76ef;
-  background-color: #0b76ef;
+/* для элемента label, связанного с .custom-checkbox */
+.custom-checkbox>span {
+  display: inline-flex;
+  align-items: center;
+  user-select: none;
 }
 
+/* создание в label псевдоэлемента before со следующими стилями */
+.custom-checkbox>span::before {
+  content: '';
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  flex-shrink: 0;
+  flex-grow: 0;
+  border: 1px solid #adb5bd;
+  border-radius: 0.25em;
+  margin-right: 0.5em;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 50% 50%;
+}
 
-//.custom-checkbox+label {
-//  display: inline-flex;
-//  align-items: center;
-//  user-select: none;
-//}
-//.custom-checkbox+label::before {
-//  content: '';
-//  display: inline-block;
-//  width: 1em;
-//  height: 1em;
-//  flex-shrink: 0;
-//  flex-grow: 0;
-//  border: 1px solid #adb5bd;
-//  border-radius: 0.25em;
-//  margin-right: 0.5em;
-//  background-repeat: no-repeat;
-//  background-position: center center;
-//  background-size: 50% 50%;
-//}
+/* стили при наведении курсора на checkbox */
+.custom-checkbox>input:not(:disabled):not(:checked)+span:hover::before {
+  border-color: #b3d7ff;
+}
 
-.custom-checkbox:checked+label::before {
+/* стили для активного чекбокса (при нажатии на него) */
+.custom-checkbox>input:not(:disabled):active+span::before {
+  background-color: #b3d7ff;
+  border-color: #b3d7ff;
+}
+
+/* стили для чекбокса, находящегося в фокусе */
+.custom-checkbox>input:focus+span::before {
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
+
+/* стили для чекбокса, находящегося в фокусе и не находящегося в состоянии checked */
+.custom-checkbox>input:focus:not(:checked)+span::before {
+  border-color: #80bdff;
+}
+
+/* стили для чекбокса, находящегося в состоянии checked */
+.custom-checkbox>input:checked+span::before {
   border-color: #0b76ef;
   background-color: #0b76ef;
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
 }
+
+/* стили для чекбокса, находящегося в состоянии disabled */
+.custom-checkbox>input:disabled+span::before {
+  background-color: #e9ecef;
+}
+
 .section-list {
   padding: 40px;
 }
