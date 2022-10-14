21add3c71948a8487f01ba680231d30c46d78cd7
timazlo <timazloy0444@yandex.ru>
2022-10-14 22:57:18 +0300

    fix: checkbox

--- src/pages/Bot.vue
+++ src/pages/Bot.vue
@@ -2,12 +2,10 @@
   <div class="container">
     <div class="page__bot bot-page">
       <div class="bot-page__column section-list">
-        <div class="section-list__check">
-          <label class="check-all custom-checkbox">
-            <input class="check-all__checkbox" type="checkbox">
-            <span class="check-all__text">Все сотрудники</span>
-          </label>
-        </div>
+        <label class="section-list__check check-all custom-checkbox">
+          <input class="check-all__checkbox" type="checkbox">
+          <span class="check-all__text">Все сотрудники</span>
+        </label>
         <Accordion
             v-for="(item, i) in employees"
             :accordion="item"
@@ -103,21 +101,16 @@ export default {
 .custom-checkbox>span::before {
   content: '';
   display: inline-block;
-  width: 1em;
-  height: 1em;
   flex-shrink: 0;
   flex-grow: 0;
-  border: 1px solid #adb5bd;
+  border: 2px solid #DFDFDF;
   border-radius: 0.25em;
   margin-right: 0.5em;
   background-repeat: no-repeat;
   background-position: center center;
   background-size: 50% 50%;
-}
-
-/* стили при наведении курсора на checkbox */
-.custom-checkbox>input:not(:disabled):not(:checked)+span:hover::before {
-  border-color: #b3d7ff;
+  width: 22px;
+  height: 22px;
 }
 
 /* стили для активного чекбокса (при нажатии на него) */
@@ -126,29 +119,20 @@ export default {
   border-color: #b3d7ff;
 }
 
-/* стили для чекбокса, находящегося в фокусе */
-.custom-checkbox>input:focus+span::before {
-  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
-}
-
-/* стили для чекбокса, находящегося в фокусе и не находящегося в состоянии checked */
-.custom-checkbox>input:focus:not(:checked)+span::before {
-  border-color: #80bdff;
-}
-
 /* стили для чекбокса, находящегося в состоянии checked */
 .custom-checkbox>input:checked+span::before {
-  border-color: #0b76ef;
-  background-color: #0b76ef;
-  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
-}
-
-/* стили для чекбокса, находящегося в состоянии disabled */
-.custom-checkbox>input:disabled+span::before {
-  background-color: #e9ecef;
+  border-color: #009AF1;
+  background-color: #009AF1;
+  background-image: url('/public/img/checkbox2.svg');
+  background-size: auto;
+  background-position-x: 3px;
 }
 
 .section-list {
   padding: 40px;
+
+  &__check {
+    margin: 0 0 30px 7px;
+  }
 }
 </style>
